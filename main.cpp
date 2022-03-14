6a3692605f3772ae5aa86ce311d87ae0329c3382
logbaby <96885996+loganbaby@users.noreply.github.com>
2022-03-14 19:05:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+
+template <typename T>
+T middleValue(const std::vector<T>& arr) {
+  if (arr.empty()) throw std::exception();
+
+  try {
+    T value = 0;
+    for (int i = 0; i < arr.size(); i++) {
+      value = value + arr[i];
+    }
+
+    return value / static_cast<T>(arr.size());
+  } catch (const std::exception x) {
+    std::cerr << "Emty vector";
+  }
+}
+
+int main() {
+  std::vector<double> vec{2.3, 5.44};
+  std::cout << middleValue(vec) << std::endl;
+
+  std::vector<int> vec_2{3, 6, 0};
+  std::cout << middleValue(vec_2) << std::endl;
+
+  std::vector<float> vec_empty;
+  std::cout << middleValue(vec_empty);
+}
